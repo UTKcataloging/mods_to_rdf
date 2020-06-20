b0a960d08bb11e077f3f9b25c047974eda2d11d6
Mark Baggett <mbagget1@utk.edu>
2020-06-20 11:35:18 -0400

    Test what happens in sphinxemoji is removed.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,6 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.imgmath',
     'sphinx.ext.viewcode',
-    'sphinxemoji.sphinxemoji',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
