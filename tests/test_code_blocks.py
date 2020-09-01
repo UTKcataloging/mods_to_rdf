c6ef66f25a8b7e9d86bdddc576ac62ee6228ba5e
Mark Baggett <mbagget1@utk.edu>
2020-09-01 15:54:39 -0400

    Refactor and recursively test.

--- tests/test_code_blocks.py
+++ tests/test_code_blocks.py
@@ -1,25 +1,49 @@
 from rdflib import Graph
+import os
 
-lines = []
-code_blocks = []
-with open('docs/source/contents/4_draft_mapping.rst', 'r') as draft:
-    for line in draft:
-        lines.append(line)
 
-i = 0
-for line in lines:
-    i += 1
-    if line.startswith('.. code-block:: turtle'):
-        new_block = ""
-        #print(f'New block on line {i}')
-        code_block_i = i + 1
-        try:
-            while lines[code_block_i].startswith("\n") or lines[code_block_i].startswith("\t") or lines[code_block_i].startswith(" "):
-                # print(f'{code_block_i}: {lines[code_block_i]}')
-                new_block += lines[code_block_i]
-                code_block_i += 1
-        except IndexError:
-            pass
-        code_blocks.append(new_block)
-for block in code_blocks:
-    Graph().parse(data=block, format="ttl")
+class TurtleTester:
+    def __init__(self, filepath: str):
+        self.lines = self.__walk_file(filepath)
+        self.code_blocks = self.__get_code_blocks()
+
+    @staticmethod
+    def __walk_file(filepath):
+        with open(filepath, "r") as current_file:
+            return [lines for lines in current_file]
+
+    def __get_code_blocks(self):
+        code_blocks = []
+        i = 0
+        for line in self.lines:
+            i += 1
+            if line.startswith(".. code-block:: turtle"):
+                new_block = ""
+                code_block_i = i + 1
+                try:
+                    while (
+                        self.lines[code_block_i].startswith("\n")
+                        or self.lines[code_block_i].startswith("\t")
+                        or self.lines[code_block_i].startswith(" ")
+                    ):
+                        new_block += self.lines[code_block_i]
+                        code_block_i += 1
+                except IndexError:
+                    pass
+                code_blocks.append(new_block)
+        return code_blocks
+
+    def test_turtle_blocks(self):
+        for block in self.code_blocks:
+            Graph().parse(data=block, format="ttl")
+        return
+
+
+if __name__ == "__main__":
+    directories_in_doc_source = ("contents", "top_level_elements", "working_docs")
+    for directory in directories_in_doc_source:
+        for root, dirs, files in os.walk(f'docs/source/{directory}'):
+            for name in files:
+                if name.endswith('.rst'):
+                    x = TurtleTester(f'{root}/{name}')
+                    x.test_turtle_blocks()
